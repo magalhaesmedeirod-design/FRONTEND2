6f8fde6fbbb80a15b0a5bcc4cff1a47825d12f2e
magalhaesmedeirod-design <magalhaesmedeirod@gmail.com>
2026-08-06 17:39:21 -0400

    a.,msdhipwerhgheojtrwg

--- src/app/contato/page.js
+++ src/app/contato/page.js
@@ -1,9 +1,18 @@
 import styles from './page.module.css';
 
 export default function Sobre() {
+    let msg = 'Entre em contato: ';
+    let cont = 0;
+    const somar() => {
+        cont++;
+        console.log(cont);
+    }
     return (
         <main>
             <h1>Contato</h1>
+            <p>{msg}</p>
+            <p>{cont}</p>
+            <buttonon onClicl={somar()}>Somar</buttonon>
         </main>
     )
 }
