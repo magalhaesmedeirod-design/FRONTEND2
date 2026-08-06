65ecc9621a23ac7e18c94438d16394b40ada6449
magalhaesmedeirod-design <magalhaesmedeirod@gmail.com>
2026-08-06 17:21:46 -0400

    llllll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,15 @@ import styles from './header.module.css';
 import Link from 'next/link';
 
 export default function Header() {
+
+    const menu = ['/', '/sobre', '/contato']
     return (
-        <header>
+        <header className={styles.header}>
             <nav>
                 <ul>
-                    <li><Link href='/home'>Home</Link></li>
-                    <li><Link href='/sobre'>Sobre</Link></li>
-                    <li><Link href='/contato'>Contato</Link></li>
+                    <li><Link href={menu[0]}>Home</Link></li>
+                    <li><Link href={menu[1]}>Sobre</Link></li>
+                    <li><Link href={menu[2]}>Contato</Link></li>
                     
                 </ul>
             </nav>
